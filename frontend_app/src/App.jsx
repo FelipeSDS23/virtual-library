dfa80efa938717a5f80db5b0bc2598eff3e96166
FelipeSDS23 <felipe03san@gmail.com>
2025-04-02 17:52:33 -0300

    Layout ajustado

--- frontend_app/src/App.jsx
+++ frontend_app/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import './App.css';
 import Layout from "./components/Layout";
 
@@ -8,6 +7,12 @@ function App() {
       <div>
         <h2>Bem-vindo ao Meu Site!</h2>
         <p>Este é um exemplo de layout em React com CSS puro.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet quibusdam, exercitationem numquam, quasi quod veritatis rerum fugit ipsa veniam aspernatur excepturi doloremque unde pariatur in iste quo distinctio nemo praesentium!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet quibusdam, exercitationem numquam, quasi quod veritatis rerum fugit ipsa veniam aspernatur excepturi doloremque unde pariatur in iste quo distinctio nemo praesentium!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet quibusdam, exercitationem numquam, quasi quod veritatis rerum fugit ipsa veniam aspernatur excepturi doloremque unde pariatur in iste quo distinctio nemo praesentium!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet quibusdam, exercitationem numquam, quasi quod veritatis rerum fugit ipsa veniam aspernatur excepturi doloremque unde pariatur in iste quo distinctio nemo praesentium!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet quibusdam, exercitationem numquam, quasi quod veritatis rerum fugit ipsa veniam aspernatur excepturi doloremque unde pariatur in iste quo distinctio nemo praesentium!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet quibusdam, exercitationem numquam, quasi quod veritatis rerum fugit ipsa veniam aspernatur excepturi doloremque unde pariatur in iste quo distinctio nemo praesentium!</p>
       </div>
     </Layout>
   );
