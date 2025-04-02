839ed8f83705dcfcc6cacb9bba005dba56163e19
FelipeSDS23 <felipe03san@gmail.com>
2025-04-02 16:57:31 -0300

    Criado footer header e sidebar

---
+++ frontend_app/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p>&copy; 2025 Meu Site. Todos os direitos reservados.</p>
+        </footer>
+    );
+};
+
+export default Footer;  
---
+++ frontend_app/src/components/Sidebar.jsx
@@ -0,0 +1,13 @@
+const Sidebar = () => {
+    return (
+        <aside>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </aside>
+    );
+};
+
+export default Sidebar;
