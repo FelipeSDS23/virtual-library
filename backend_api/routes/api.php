bcb0c388bb620a9ecccac2cd4464b04b1c36ba9d
FelipeSDS23 <felipe03san@gmail.com>
2025-04-07 18:19:05 -0300

    Criadas rotas AuthController

--- backend_api/routes/api.php
+++ backend_api/routes/api.php
@@ -4,13 +4,16 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
-Route::middleware('auth:sanctum')->prefix('user')->group(function () {
-    Route::get('/', [UserController::class, 'show']);
+Route::prefix('user')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
 
 Route::apiResource('books', BookController::class);
